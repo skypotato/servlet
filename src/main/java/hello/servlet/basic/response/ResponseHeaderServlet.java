709033c8d744ce752fcbeb0e4bb53c1491ddc9b9
skypotato <hunsooski@naver.com>
2021-10-20 02:25:22 +0900

    2021.10.20 : HttpServletResponse - 기본 사용법 #2

--- src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -22,7 +22,19 @@ public class ResponseHeaderServlet extends HttpServlet {
         response.setHeader("Pragma", "no-cache");
         response.setHeader("my-header", "hello");
 
+        //[Header 편의 메서드]
+        content(response);
+
         PrintWriter writer = response.getWriter();
         writer.write("ok");
+    }
+
+    private void content(HttpServletResponse response){
+        //Content-Type: text/plain;charset=utf-8
+        //Content-Length: 2
+        //response.setHeader("Content-Type", "text/plain;charset=utf-8";
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        // response.setContentLength(2); //(생략시 자동 생성)
     }
 }

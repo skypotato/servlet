9139fa87e3f8e7ce7b46b74ad87018e692a0d7c3
skypotato <hunsooski@naver.com>
2021-10-21 23:37:17 +0900

    2021.10.21 : 추가적으로 Mutipart로 파일 업로드를 구현해보려고 한다.

--- src/main/java/hello/servlet/basic/request/RequestMultipartServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestMultipartServlet.java
@@ -7,8 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "requestMutipartServlet", urlPatterns = "request-mutipart")
-public class RequestMutipartServlet extends HttpServlet {
+@WebServlet(name = "requestMultipartServlet", urlPatterns = "request-multipart")
+public class RequestMultipartServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("[전체 파라미터 조회] - start");

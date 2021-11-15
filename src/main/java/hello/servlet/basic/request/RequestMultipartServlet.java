90635cd498c52ad8333649ad04ce779bd7cb58aa
skypotato <hunsooski@naver.com>
2021-11-15 22:40:12 +0900

    2021.11.15 : Multipart Api 구현하기

--- src/main/java/hello/servlet/basic/request/RequestMultipartServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestMultipartServlet.java
@@ -1,16 +1,15 @@
 package hello.servlet.basic.request;
 
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
+
 
 @WebServlet(name = "requestMultipartServlet", urlPatterns = "/request-multipart")
 public class RequestMultipartServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.getHeader("");
     }
 }

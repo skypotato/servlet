b26bff8c09dddfb40f2a9a0cfc4b2298800e0ebe
skypotato <hunsooski@naver.com>
2022-01-15 22:50:35 +0900

    2022.01.15 : MVC 패턴 적용 #10

--- src/main/java/hello/servlet/web/servletmvc/MemberFormServlet.java
+++ src/main/java/hello/servlet/web/servletmvc/MemberFormServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name="mvcMamberFormServlet", urlPatterns = "/servelt-mvc/members/new-form")
+@WebServlet(name="mvcMamberFormServlet", urlPatterns = "/servlet-mvc/members/new-form")
 public class MemberFormServlet extends HttpServlet {
 
     @Override

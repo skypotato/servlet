671a81bf77c6e3870b312e59d0b93b0b2a698e41
skypotato <hunsooski@naver.com>
2022-05-28 16:37:59 +0900

    2022.05.28 : 스프링 MVC 실용적인 방식 #4

--- src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -5,6 +5,7 @@ import hello.servlet.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -29,7 +30,7 @@ public class SpringMemberControllerV3 {
         return "members";
     }
 
-    @GetMapping("/save")
+    @PostMapping("/save")
     public String save(
             @RequestParam("username") String username,
             @RequestParam("age") int age,

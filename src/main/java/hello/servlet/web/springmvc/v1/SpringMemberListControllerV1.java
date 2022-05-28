cbc1d54fc49d4b8137480add9ab4201135a90fda
skypotato <hunsooski@naver.com>
2022-05-28 16:11:15 +0900

    2022.05.28 : 스프링 MVC 시작하기 #4

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class SpringMemberListControllerV1 {
@@ -15,7 +14,7 @@ public class SpringMemberListControllerV1 {
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @RequestMapping("/springmvc/v1/members")
-    public ModelAndView process(Map<String, String> paramMap) {
+    public ModelAndView process() {
         List<Member> members = memberRepository.findAll();
 
         ModelAndView mv = new ModelAndView("members");
